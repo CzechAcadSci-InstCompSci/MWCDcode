8a342acc55868467ae89fbd85aa62e62e76ee356
honza@resaizer.com <123698745babucha>
2019-11-13 15:40:59 +0100

    file addition

---
+++ mainMwcd.m
@@ -0,0 +1,87 @@
+### Computation of the minimum weighted covariance determinant estimator (MWCD).
+### This is the main file. 
+### The input data file has to fulfil:
+### - format *.txt
+### - individual observations in rows
+### - values separated by spaces
+### - the first column corresponds to the index of the observation (i=1,...,n)
+### - e.g. 2 5 5 5 in the input file corresponds to the second observation equal to (5,5,5) 
+### - we assume the data matrix to be regular
+
+clear all;
+
+% canContinue = false;
+% while(~canContinue) 
+%     try
+%         inputData = input('Please enter the name of the data file: ','s');
+%         data = load(inputData);
+%     catch
+%         'Error in reading the data file, please try again.'
+%         continue;
+%     end
+%     [N, dim] = size(data);
+%     
+%     if(prod(svd(data)) ~= 0)
+%         canContinue = true;
+%     else
+%         'The requirement of regularity of the data matrix is violated!!!'
+%     end
+% end
+curX = 2;
+curY = 3;
+data = load('dataHBK.txt');
+%data = data*[-2 1; 1 -1];
+data = data(:, 2:5);
+[T, C] = mwcd(data, 0);
+%(X-T)TC(X-T) = CONST
+CONST = 5;
+C = inv(C);
+[V,D] = eigs(C)
+
+Nfit = 100;
+fi = linspace(0, 2*pi, Nfit);
+y1 = sqrt(CONST/D(curX,curX))*cos(fi);
+y2 = sqrt(CONST/D(curY,curY))*sin(fi);
+y = [y1; y2]
+mwcdFit = repmat(T, 1, Nfit)+ V(:,[curX,curY])*y;
+
+[T, C] = mwcd(data, 1);
+%(X-T)TC(X-T) = CONST
+CONST = 5;
+C = inv(C);
+[V,D] = eigs(C)
+
+Nfit = 100;
+fi = linspace(0, 2*pi, Nfit);
+y1 = sqrt(CONST/D(curX,curX))*cos(fi);
+y2 = sqrt(CONST/D(curY,curY))*sin(fi);
+y = [y1; y2]
+classFit = repmat(T, 1, Nfit)+ V(:,[curX,curY])*y;
+
+% pom = sqrt(-4*C(1,1)*CONST/((C(2,1)+C(1,2))^2 - 4*C(1,1)*C(2,2)))
+% mwcdFitX = linspace(T(2)-pom,T(2)+pom,100);
+% mwcdFitY1 = (-(C(2,1)+C(1,2)*(mwcdFitX - T(1)) - sqrt(((C(2,1)+C(1,2))^2)*(mwcdFitX - T(1)).^2 - 4*C(1,1)*(C(2,2)*((mwcdFitX - T(1)).^2)-CONST))))/(2*C(1,1))+T(1);
+% mwcdFitY2 = (-(C(2,1)+C(1,2)*(mwcdFitX - T(1)) + sqrt(((C(2,1)+C(1,2))^2)*(mwcdFitX - T(1)).^2 - 4*C(1,1)*(C(2,2)*((mwcdFitX - T(1)).^2)-CONST))))/(2*C(1,1))+T(1);
+
+options.intercept = 0;
+options.alpha = 0.75;
+options.ntrial = 500;
+%[sig,mu] = fastmcd(data, options)
+%[mcdV,mcdD] = eigs(sig.cov);
+% mcdy = [sqrt(CONST/mcdD(1,1))*cos(fi); sqrt(CONST/mcdD(2,2))*sin(fi)];
+% mcdFit = repmat(sig.center', 1, Nfit)+ mcdV*mcdy;
+
+hold on;
+data = data';
+scatter(data(2, :),data(1, :));
+plot(mwcdFit(2, :), mwcdFit(1, :), 'color', 'black');
+plot(classFit(2, :), classFit(1, :), 'color', 'green');
+legend('body', 'MWCD', 'Klasický odhad');
+xlabel('x_2');
+ylabel('x_3');
+myboldify;
+% plot(mcdFit(2, :), mcdFit(1, :), 'gx');
+% plot(mwcdFitX, mwcdFitY1);
+% plot(mwcdFitX, mwcdFitY2);
+hold off;
+
---
+++ mwcdCheck.m
@@ -0,0 +1,60 @@
+### The function mwcdCheck() computes the MWCD estimator for the given dataset.
+
+function [T,CM] = mwcd(data)
+[N, dim] = size(data);
+data = data';
+weight = ones(1, N) - ((1:N)-1)/N;
+%weight = ones(1, N);
+sumWeight = sum(weight);
+theBestC = 0;
+theBestT = 0;
+theBestCR = 1000000000000000;
+p = N-2;
+s = RandStream('mt19937ar','Seed',0);
+
+for i=1:1000
+    ps = randperm(s, N, p);
+	A = data(1:dim, ps);
+    t = mean(A, 2);
+    C = zeros(dim, dim);
+    for j=1:p
+        C = C + (A(:, j)-t)*(A(:, j)-t)';
+    end 
+    C = C/p;
+    c1= 100000;
+    
+    while true
+        residues = zeros(N, 1);
+        C1 = inv(C);
+        for j=1:N
+            residues(j, 1) = (data(:, j)-t)'*C1*(data(:, j)-t);
+        end 
+        J = 1:N;
+        [kk, I] = sort(abs(residues));
+        J(I) = J;
+        newWeight = weight(J); 
+        for j=1:N
+            t = t + data(:, j)*newWeight(j);
+        end
+        t = t/sumWeight;
+        C = zeros(dim, dim);
+        for j=1:p
+            C = C + (data(:, j)-t)*(data(:, j)-t)'*newWeight(j);
+        end 
+        C = C/sumWeight;
+        c2 = det(C)
+        if(c2 < c1)
+           c1 = c2;
+        else
+            if(c2 < theBestCR)
+                theBestCR = c2;
+                theBestC = C;
+                theBestT = t;
+            end
+            break;
+        end
+    end
+end
+CM = theBestC;
+T = theBestT;
+end
